d02abf54df2fb2684777d2339c43fe8ae1a7940d
mdeshmukh1111-hub <mdeshmukh2006@gmail.com>
2026-04-28 18:40:59 +0000

    fixing it

--- Alert.java
+++ Alert.java
@@ -1,13 +1,29 @@
-public abstract class Alert {
-    String stockSymbol;
-    double targetPrice;
-    boolean isActive;
 
-Alert(String stockSymbol, double targetPrice){
-    this.stockSymbol=stockSymbol;
+public abstract class Alert {
+   private Stock stock;
+   private double targetPrice;
+   private boolean isActive;
+
+Alert(Stock stock, double targetPrice){
+    this.stock=stock;
     this.targetPrice=targetPrice;
     this.isActive=true;
 }
-abstract void checkAndTrigger(currentPrice);
-
+double currentPrice=stock.getcurrentPrice();
+abstract void checkAndTrigger(double currentPrice);
+Stock getStockSymbol(){
+    return stock;
+}
+double getTargetPrice(){
+    return targetPrice;
+}
+boolean isActive(){
+    return isActive;
+}
+boolean deactivate(){
+    return isActive=false;
+}
+String TooString(){
+return "[" + getStockSymbol() + " | Target: " + targetPrice +" | Active: " + isActive + "]";
+}
 }
